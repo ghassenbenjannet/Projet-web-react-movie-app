2da3f4d39b49a10466cb025a00767da626ef68d5
Hamza SALAH <contact.halaspec@gmail.com>
2023-11-21 10:44:05 +0100

    Add Movie Detail query feature

---
+++ my-react-app/src/infrastructure/queries/MovieDetailQuery.ts
@@ -0,0 +1,24 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { MOVIE_DETAIL_ID_ENDPOINT } from '../../infrastructure/config/Endpoints';
+import { tmdb_api_acces } from '../config/TMDBConfig';
+
+export const useFetchMovieDetailQuery = (id: number) => {
+  if (id === undefined) {
+    return {
+      data: undefined,
+      error: 'Movie ID is undefined',
+      isLoading: false,
+    };
+  }
+
+  const { apiKey } = tmdb_api_acces; 
+  return useQuery(['movieDetail', id], async () => {
+      const response = await axios.get(`${MOVIE_DETAIL_ID_ENDPOINT}${id}`, {
+        params: {
+          api_key: apiKey,
+        },
+      });
+      return response.data;
+    })
+};
