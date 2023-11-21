01b598d506a996a1c2fbaf4cbb48c1c07c9eeb14
BEN JANNET Ghassen <ghassen.b.j.15@gmail.com>
2023-11-21 11:31:31 +0100

    routing detail and fav

--- my-react-app/src/routing/Router.tsx
+++ my-react-app/src/routing/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { MovieList } from '../components/organisms/MovieList/MovieList';
 import { ErrorPage } from './ErrorPage';
+import { MovieDetailsPage } from '../components/organisms/MovieDetails/MovieDetails';
 
 
 export const Router: React.FC = () => {
@@ -10,6 +11,8 @@ export const Router: React.FC = () => {
       <Route path="/" element={<Navigate to="/movies_list" />} />
       <Route path="/movies_list" element={<MovieList />} />
       <Route path="/error" element={<ErrorPage />} />
+      <Route path="/movies_list" element={<MovieList />} />
+      <Route path="/movie/:id" element={<MovieDetailsPage />} />
     </Routes>
   );
 };
--- my-react-app/src/routing/Routes.ts
+++ my-react-app/src/routing/Routes.ts
@@ -1,4 +1,6 @@
 export const routes = {
     moviesList: '/movies_List',
     erreur: '/error',
+    movieDetail: '/movie/:id',
+    favoris: '/favorites'
   };
