45cf949d7347a48771d4d52ce3a0424146c1dc31
Ghassen Ben Jannet <ghassenbenjannet1@gmail.com>
2023-11-23 15:49:35 +0100

    search

--- my-react-app/src/infrastructure/config/Endpoints.ts
+++ my-react-app/src/infrastructure/config/Endpoints.ts
@@ -3,3 +3,4 @@ export const POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500";
 export const MOVIE_IMAGES_ENDPOINT = "https://api.themoviedb.org/3/movie/{movie_id}/images";
 export const MOVIE_CREDIT_ENDPOINT = "https://api.themoviedb.org/3/movie/{movie_id}/credits";
 export const MOVIE_DETAIL_ID_ENDPOINT = "https://api.themoviedb.org/3/movie/";
+export const SEARCH_MOVIE_ENDPOINT="https://api.themoviedb.org/3/search/movie"
